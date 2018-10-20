13546be0677ab3381c4c15a5b45c3d26d16e2117
Trinity Statum <tj.statum@gmail.com>
2018-10-20 12:01:13 -0500

    function example

---
+++ maths.js
@@ -0,0 +1,15 @@
+var maths = {
+    add: function(x,y){
+        return x+y;
+    }, 
+    subtract: function(x,y){
+        return x-y;
+    },
+    divide: function(x,y){
+        return x/y;
+    },
+    mulitply: function(x,y){
+        return x*y;
+    }
+}
+
